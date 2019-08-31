696e95597a8dc94bb9ea9a90a7b55000548a965b
Slavisha84 <slavoljub.petkovic@outlook.com>
2019-08-31 18:45:30 -0400

    updating function BMI
    Signed-off-by: Slavisha84 <slavoljub.petkovic@outlook.com>

--- app/src/main/java/com/example/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.fitnesstracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText height;
+    private EditText weight;
+    private EditText age;
+    private TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        height = (EditText) findViewById(R.id.height);
+        weight = (EditText) findViewById(R.id.weight);
+        age = (EditText) findViewById(R.id.age);
+        result = (TextView) findViewById(R.id.result);
+    }
+    public void calculateBMI(View v) {
+        String heightStr = height.getText().toString();
+        String weightStr = weight.getText().toString();
+
+        if (heightStr != null && !"".equals(heightStr)&& weightStr != null && !"".equals(weightStr)) {
+            float heightValue = Float.parseFloat(heightStr)/100;
+            float weightValue = Float.parseFloat(weightStr);
+
+            float bmi =weightValue / (heightValue*heightValue);
+            result.setText("The result is: " + bmi);
+        }
     }
 }
